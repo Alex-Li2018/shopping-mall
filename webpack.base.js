e5bd256506d19324bbd3286a2a80200d3b6aa1cc
Alex-Li2018 <alexstudyweb@163.com>
2019-07-27 22:50:29 +0800

    "完善架子的功能,实现eslint校验与babel转换

--- webpack.base.js
+++ webpack.base.js
@@ -6,28 +6,60 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin'); //vue-loader15.*之后
 module.exports = {
     entry: './src/main.js',
     output: {
-        filename: 'main.js',
+        filename: 'app.js',
         path: path.resolve(__dirname,'dist')
     },
     module: {
         //loader的执行顺序是从下到上,从右到左
         rules: [
-            //解析vue文件,会将template,script,css,sass,less这三种分割开来
+            // js语法规范
             {
-                test: /.vue$/,
-                loader: 'vue-loader'
+                test: /\.js$/,
+                use: [
+                   {
+                        loader: 'eslint-loader',
+                        options: {
+                            enforce: 'pre' //前置loader pre 后置loader post 
+                        }
+                   }
+                ] 
+            },
+            //es6的语法处理
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            //预设 用babel把es6转为es5
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                ["@babel/plugin-proposal-class-properties", { "loose": true }], //支持class语法
+                                "@babel/plugin-transform-runtime", //支持promise的语法 Generator语法
+                                "@babel/plugin-syntax-dynamic-import" //支持import()函数
+                            ]
+                        }
+                    }
+                ],
+                include: path.resolve(__dirname,'src'), //包括src下面的js
+                exclude: /node_modules/ //排除那些js 
             },
             //解析css文件,因为webpack只能打包js,所以需要loader来实现打包css
             //style-loader,将打包的css以style标签的形势插入页面
             {
-                test: /.css$/,
+                test: /\.css$/,
                 loader: ['style-loader','css-loader']
             },
             //处理scss文件
             {
-                test: /.scss$/,
+                test: /\.scss$/,
                 loader: ['style-loader','css-loader',"sass-loader"] 
-            }
+            },
+            //解析vue文件,会将template,script,css,sass,less这三种分割开来
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
         ]
     },
     plugins: [
